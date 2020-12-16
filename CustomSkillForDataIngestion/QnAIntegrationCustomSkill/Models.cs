4f2448c220b0c45809a64c26f7a1f5cb26c945fa
Derek Legenzoff <dereklegenzoff@gmail.com>
2020-12-15 21:38:39 -0800

    adding search function

---
+++ CustomSkillForDataIngestion/QnAIntegrationCustomSkill/Models.cs
@@ -0,0 +1,13 @@
+﻿using Azure.Search.Documents.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QnAIntegrationCustomSkill
+{
+    class SearchOutput
+    {
+        public long? count { get; set; }
+        public List<SearchResult<SearchDocument>> results { get; set; }
+    }
+}
