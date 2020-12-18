7c190972020b3f0c97c419dc482680c3bc0e5c87
Derek Legenzoff <dereklegenzoff@gmail.com>
2020-12-17 17:27:33 -0800

    adding docs for single endpoint and removing stopwords from search

--- CustomSkillForDataIngestion/QnAIntegrationCustomSkill/Search.cs
+++ CustomSkillForDataIngestion/QnAIntegrationCustomSkill/Search.cs
@@ -27,7 +27,7 @@ namespace QnAIntegrationCustomSkill
         private static string searchApiKey = Environment.GetEnvironmentVariable("SearchServiceApiKey", EnvironmentVariableTarget.Process);
         private static string searchServiceName = Environment.GetEnvironmentVariable("SearchServiceName", EnvironmentVariableTarget.Process);
         private static string searchIndexName = Constants.indexName;
-       
+
         private static string storageAccountName = Environment.GetEnvironmentVariable("StorageAccountName", EnvironmentVariableTarget.Process);
         private static string storageAccountKey = Environment.GetEnvironmentVariable("StorageAccountKey", EnvironmentVariableTarget.Process);
 
@@ -110,7 +110,7 @@ namespace QnAIntegrationCustomSkill
             options.Select.Add("metadata_storage_path");
             options.Select.Add("id");
 
-            var response = await searchClient.SearchAsync<SearchDocument>(q, options);
+            var response = await searchClient.SearchAsync<SearchDocument>(RemoveStopwords(q), options);
 
             Dictionary<string, IList<FacetValue>> facets = new Dictionary<string, IList<FacetValue>>();
 
@@ -139,6 +139,29 @@ namespace QnAIntegrationCustomSkill
             }
 
             return new OkObjectResult(output);
+        }
+
+        public static string RemoveStopwords(string query)
+        {
+            List<string> stopWords = new List<string>(){"a", "about", "above", "after", "again", "against", "all", "am", "an", "and", "any", "are", "aren't", "as", "at", "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", "can't", "cannot", "could", "couldn't", "did",
+              "didn't", "do", "does", "doesn't", "doing", "don't", "down", "during", "each", "few", "for", "from", "further", "had", "hadn't", "has", "hasn't", "have", "haven't", "having", "he", "he'd", "he'll", "he's", "her", "here", "here's", "hers", "herself",
+              "him", "himself", "his", "how", "how's", "i", "i'd", "i'll", "i'm", "i've", "if", "in", "into", "is", "isn't", "it", "it's", "its", "itself", "let's", "me", "more", "most", "mustn't", "my", "myself", "no", "nor", "not", "of", "off", "on", "once", "only",
+              "or", "other", "ought", "our", "ours", "ourselves", "out", "over", "own", "same", "shan't", "she", "she'd", "she'll", "she's", "should", "shouldn't", "so", "some", "such", "than", "that", "that's", "the", "their", "theirs", "them", "themselves", "then",
+              "there", "there's", "these", "they", "they'd", "they'll", "they're", "they've", "this", "those", "through", "to", "too", "under", "until", "up", "very", "was", "wasn't", "we", "we'd", "we'll", "we're", "we've", "were", "weren't", "what", "what's", "when",
+              "when's", "where", "where's", "which", "while", "who", "who's", "whom", "why", "why's", "with", "won't", "would", "wouldn't", "you", "you'd", "you'll", "you're", "you've", "your", "yours", "yourself", "yourselves" };
+
+            var words = query.ToLower().Split(' ');
+
+            List<string> outputs = new List<string>();
+            foreach (string word in words)
+            {
+                if (!stopWords.Contains(word))
+                {
+                    outputs.Add(word);
+                }
+            }
+
+            return string.Join(" ", outputs);
         }
 
         private static async Task<string> GetKbId(ILogger log)
